1c43e96bd132702dd292b6eaa362637a361a8b7b
Jat286 <73901875+Jat286@users.noreply.github.com>
2026-04-25 20:17:41 +0100

    Update server.py

--- server.py
+++ server.py
@@ -439,4 +439,4 @@ def handle_online_request(data):
 # ----------------------------------------------------
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
