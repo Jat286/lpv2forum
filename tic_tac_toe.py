b1fdc53af4970c9184898d08e7db5c8fccaee893
LP-V2 <tfsmith128@gmail.com>
2026-05-22 15:04:57 +0100

    Create tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,19 @@
+from flask_socketio import Namespace, emit, join_room, leave_room
+
+class T3Namespace(Namespace):
+  def on_connect(self):
+        print("Client connected to /tictactoe")
+
+  def on_disconnect(self):
+      print("Client disconnected from /tictactoe")
+
+  def on_join(self, data):
+      room = data["room"]
+      join_room(room)
+      emit("joined", {"room": room})
+
+  def on_move(self, data):
+      emit("move", data, room=data["room"])
+
+  def on_reset(self, data):
+      emit("restart", data, room=data["room"])
