5d45066aa06d95b182f10211d1a8603a6dcd154c
LP-V2 <tfsmith128@gmail.com>
2026-08-31 18:08:27 +0100

    Update server.py

--- server.py
+++ server.py
@@ -104,12 +104,12 @@ def require_auth():
 @socketio.on("connect")
 def handle_auth(auth=None):
     if not auth or not isinstance(auth, dict):
-        pass
+        return False
         
     clientID = auth.get("id", "")
 
     if clientID not in clientPublicKeys.keys():
-        return False  # disconnect client
+        pass
 
     challenge = os.urandom(32)
     challenges[request.sid] = {"challenge" : challenge, "id" : clientID}
