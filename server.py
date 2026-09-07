6c6b408d225998aa75dd0b1640c9a999b5184109
LP-V2 <tfsmith128@gmail.com>
2026-09-07 11:19:59 +0100

    Update server.py

--- server.py
+++ server.py
@@ -129,8 +129,7 @@ def get_completed_uploads():
 
 @socketio.event
 def files():
-    sid = request.sid
-    emit_sid("server_uploads", {"files" : get_completed_uploads()}, to=sid)
+    emit_sid("server_uploads", {"files" : get_completed_uploads()}, to=request.sid)
 
 @socketio.event
 def upload_chunk(payload):
